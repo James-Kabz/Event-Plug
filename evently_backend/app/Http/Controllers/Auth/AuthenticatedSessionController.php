2e5c233ef24d5dfe286708fee958f5d829fc40fa
James-Kabz <kabogp@gmail.com>
2025-03-13 22:18:23 +0300

    Added laravel breeze for authentication and also roles and permissions

---
+++ evently_backend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): Response
+    {
+        $validatedData = $request->validate([
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required', 'string'],
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if(!Auth::attempt($validatedData)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Login failed. Please check your credentials.'
+            ], 401);
+        }
+
+        $token = $user->createToken($user->name)->plainTextToken;
+        
+        return response()->json([
+            'success' => true,
+            'user' => $user,
+            'access_token' => $token
+        ], 200);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): Response
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return response()->noContent();
+    }
+}
